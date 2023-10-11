1fa13174748f7e643b51eb7a4737ca390cbbc7d4
Stas <stanislav.kovalenko.dev@gmail.com>
2023-10-11 16:10:52 +0200

    remove extra logs

--- app.js
+++ app.js
@@ -5,21 +5,6 @@ function init() {
         formats: ["upc_a", "upc_e"],
     } );
 
-    barcodeDetector.addEventListener("load", ({ detail }) => {
-        const details = document.getElementById( 'details' );
-
-        details.textContent = JSON.stringify( detail );
-
-        console.log(detail); // zxing wasm module
-    });
-
-    barcodeDetector.addEventListener("error", ({ detail }) => {
-        const details = document.getElementById( 'error' );
-
-        details.textContent = JSON.stringify( detail );
-        console.log(detail); // an error
-    });
-
     document.getElementById( 'uploadInput' ).addEventListener( 'change', ( e ) => uploadInputChange( barcodeDetector, e ) );
 }
 
