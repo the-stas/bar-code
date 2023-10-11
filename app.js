479c462de814ffd57c5154234fdb7ecdb2987ac9
Stas <stanislav.kovalenko.dev@gmail.com>
2023-10-11 15:31:19 +0200

    init 2

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const barcodeDetector = new window.BarcodeDetectionAPI.BarcodeDetector( {
 
  window?.BarcodeDetection?.getSupportedFormats().then((supportedFormats) => {
     console.log('supportedFormats ->');
-    supportedFormats.forEach((format) => console.log(format));
+    const supportedFormats2 = document.getElementById( 'supportedFormats' );
+    supportedFormats.forEach((format) => supportedFormats2.textContent += format);
   });
 
 barcodeDetector.addEventListener("load", ({ detail }) => {
+    const details = document.getElementById( 'details' );
+
+    details.textContent = JSON.stringify( detail );
+
   console.log(detail); // zxing wasm module
 });
 
 barcodeDetector.addEventListener("error", ({ detail }) => {
+    const details = document.getElementById( 'error' );
+
+    details.textContent = JSON.stringify( detail );
     console.log(detail); // an error
   });
 
@@ -22,21 +30,45 @@ barcodeDetector.addEventListener("error", ({ detail }) => {
     const content = event.target.result;
     console.log('readFile');
     console.log(content);
+      const details = document.getElementById( 'readFile' );
+
+      details.textContent = JSON.stringify( content );
+
+      const barcodeDetectorLog = document.getElementById( 'barcodeDetectorLog' );
+
+
     barcodeDetector.detect(content).then((barcodes) => {
-        barcodes.forEach((barcode) => console.log(barcode.rawValue));
+        barcodes.forEach((barcode) => {
+            barcodeDetectorLog.textContent += JSON.stringify( barcode.rawValue );
+            console.log(barcode.rawValue);
+        });
       })
       .catch((err) => {
+          const barcodeDetectorError = document.getElementById( 'barcodeDetectorError' );
+
+          barcodeDetectorError.textContent = err;
         console.log(err);
       });
   }
 
   function uploadInputChange( { target } ) {
     const file = target.files[0];
-    console.log(file);
+      console.log('readFile');
+      console.log(file);
+      const details = document.getElementById( 'readFile' );
+
+      details.textContent = JSON.stringify( file );
+
     barcodeDetector.detect(file).then((barcodes) => {
-        barcodes.forEach((barcode) => console.log(barcode.rawValue));
+        barcodes.forEach((barcode) => {
+            barcodeDetectorLog.textContent += JSON.stringify( barcode.rawValue );
+            console.log(barcode.rawValue);
+        });
       })
       .catch((err) => {
+          const barcodeDetectorError = document.getElementById( 'barcodeDetectorError' );
+
+          barcodeDetectorError.textContent = err;
         console.log(err);
       });
     // var reader = new FileReader();
